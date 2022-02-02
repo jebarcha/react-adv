1550597df4cfcd5ddf26f96c987a4f1289081151
jebarcha <jebarcha@hotmail.com>
2022-02-02 16:55:42 -0600

    Control Props alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -7,33 +7,20 @@ export const useShoppingCart = () => {
 
     const onProductCountChange = ( { count, product }: { count: number, product: Product } ) => {
 
+        console.log({count});
+
         setShoppingCart( oldShoppingCart => {
 
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 }
-            
-            if ( Math.max( productInCart.count + count, 0 ) > 0) {
-                productInCart.count += count;
-                return {
-                    ...oldShoppingCart,
-                    [product.id]: productInCart
-                }
+            if (count === 0) {
+                //delete ({...oldShoppingCart})[product.id];
+                const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+                return rest;
+            } 
+
+            return {
+                ...oldShoppingCart,
+                [ product.id ]: { ...product, count }
             }
-
-            // Delete the product
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            return { ...rest };
-
-            // if (count === 0) {
-            //     //delete ({...oldShoppingCart})[product.id];
-            //     const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            //     //console.log(toDelete);
-            //     return rest;
-            // } 
-
-            // return {
-            //     ...oldShoppingCart,
-            //     [ product.id ]: { ...product, count }
-            // }
 
         })
     }

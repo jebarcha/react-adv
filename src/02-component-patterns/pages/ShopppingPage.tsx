87a4928b4ac7808e62f5fa0cbaa28636ff522cb6
jebarcha <jebarcha@hotmail.com>
2022-02-04 17:09:25 -0600

    pre-npm ready

--- src/02-component-patterns/pages/ShopppingPage.tsx
+++ src/02-component-patterns/pages/ShopppingPage.tsx
@@ -1,8 +1,5 @@
-import { isJSDocSignature } from 'typescript';
 import { ProductCard, ProductButtons, ProductImage, ProductTitle } from '../components';
 import { products } from '../data/products';
-
-import '../styles/custom-styles.css';
 
 const product = products[0];
 
@@ -17,25 +14,17 @@ export const ShopppingPage = () => {
             <ProductCard 
                 key={product.id}
                 product={product}
-                className="bg-dark text-white"
                 initialValues={{
                     count: 6,
-                    maxCount: 10
+                    //maxCount: 10
                 }}
             >
                 {
                     ( { reset, count, increaseBy, maxCount, isMaxCountReached } ) => (
                         <>
-                            <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2' }}/>
-                            <ProductTitle className="text-white text-bold"/>
-                            <ProductButtons className="custom-buttons"/>
-
-                            <button onClick={reset}>Reset</button>
-                            <button onClick={ () => increaseBy(-2) }> -2 </button>
-                            {
-                                ( !isMaxCountReached && <button onClick={ () => increaseBy(2) }> +2 </button>)
-                            }
-                            <span>{count} - {maxCount}</span>
+                            <ProductImage />
+                            <ProductTitle />
+                            <ProductButtons />
                         </>
                     )
                 }
